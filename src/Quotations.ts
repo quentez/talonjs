166049fe031946a7dbe56c0861f419786874af3c
Quentin Calvez <quentin.calvez@quentez.com>
2016-04-17 20:00:48 -0700

    :wrench: Add regex collection for use in quotations.

--- src/Quotations.ts
+++ src/Quotations.ts
@@ -1,3 +1,81 @@
-export default class Test {
+/*
+ * Constants.
+ */
+
+const SplitterMaxLines = 4;
+const MaxLinesCount = 1000;
+
+/** Class operating on message bodies, trying to extract 
+ *  origional messages (without quoted messages). */
+export default class Quotations {
   
+  /*
+   * Public interface.
+   */
+  
+  extractFrom(messageBody: string, contentType: string = "text/plain"): string {
+    // Depending on the content-type, use the appropriate method.
+    switch (contentType) {
+      case "text/plain":
+        return this.extractFromPlain(messageBody);
+      case "text/html":
+        return this.extractFromHtml(messageBody);
+      default:
+        return messageBody;
+    }
+  }
+  
+  /** 
+   * Extracts a non quoted message from the provided plain text.
+   * @param {string} messageBody - The plain text body to extract the message from.
+   * @return {string} The extracted, non-quoted message.
+   */
+  extractFromPlain(messageBody: string): string {
+    const delimiter = this.findDelimiter(messageBody);
+    messageBody = this.preprocess(messageBody, delimiter);
+    
+    // Only take the X first lines.
+    let lines = messageBody.split(/\r?\n/).slice(MaxLinesCount);
+    const markers = this.markMessageLines(lines);
+    lines = this.processMarkedLines(lines, markers);
+    
+    // Concatenate the lines, change links back, strip and return.
+    messageBody = lines.join(delimiter);
+    messageBody = this.postProcess(messageBody);
+    
+    return messageBody;
+  }
+  
+  /**
+   * Extracts a non quoted message from the provided html.
+   * @param {string} messageBody - The html body to extract the message from.
+   * @return {string} The extracted, non-quoted message.
+   */
+  extractFromHtml(messageBody: string): string {
+    return messageBody;
+  }
+  
+  /*
+   * Private methods.
+   */
+  
+  private findDelimiter(messageBody: string): string {
+    
+  }
+  
+  private preprocess(messageBody: string, delimiter: string): string {
+    
+  }
+  
+  private markMessageLines(lines: string[]): string[] {
+    
+  }
+  
+  private processMarkedLines(lines: string[], markers: string[]): string[] {
+    
+  }
+  
+  private postProcess(messageBody: string): string {
+    
+  }
 }
---
+++ src/Quotations/Regexp.ts
@@ -0,0 +1,123 @@
+export const Forward = new RegExp("^[-]+[ ]*Forwarded message[ ]*[-]+$", "im");
+
+export const OnDateSomebodyWrote = new RegExp(
+  `(-*[>]?[ ]?(${
+    // Beginning of the line.
+    [
+      "On",       // English,
+      "Le",       // French
+      "W dniu",   // Polish
+      "Op",       // Dutch
+      "Am",       // German
+      "På",       // Norwegian
+      "Den"       // Swedish, Danish
+    ].join("|")
+  })[ ].*(${
+    // Date and sender separator.
+    [
+      ",",          // Most languages separate date and sender address by comma.
+      "użytkownik"  // Polish date and sender address separator.
+    ].join("|")
+  })(.*\n){0,2}.*(${
+    // Ending of the line.
+    [
+      "wrote", "sent",                    // English
+      "a écrit",                          // French
+      "napisał",                          // Polish
+      "schreef", "verzond", "geschreven", // Dutch
+      "schrieb",                          // German
+      "skrev"                             // Norwegian, Swedish
+    ].join("|")    
+  }):?-*)`
+);
+
+export const OnDateWroteSomebody = new RegExp(
+  `(-*[>]?[ ]?(${
+    // Beginning of the line.
+    [
+      "Op", 
+      "Am"  // German
+    ].join("|")
+  })[ ].*(.*\n){0,2}.*(${
+    // Ending of the line.
+    [
+      "schreef", "verzond", "geschreven", // Dutch
+      "schrieb"                           // German
+    ].join("|")
+  })[ ]*.*:)`
+);
+
+export const Quotation = new RegExp(
+  "(" +
+    // Quotation border: splitter line or a number of quotation marker lines.
+    "(?:" +
+      "s" +
+      "|" + 
+      "(?:me*){2,}" +
+    ")" +
+    // Quotation lines could be marked as splitter or text, etc.
+    ".*" +
+    // But we expect it to end with a quotation marker line.
+    "me*" +
+  ")" +
+   
+  // After quotations should be text only or nothing at all.
+  "[te]*$"
+);
+
+export const EmptyQuotation = new RegExp(
+  "(" +
+    // Quotation border: splitter line or a number of quotation marker lines.
+    "(?:" +
+      "(?:se*)+" +
+      "|" +
+      "(?:me*){2,}" +
+    ")" +
+  ")" +
+  "e*"
+);
+
+// ------Original Message------ or ---- Reply Message ----
+// With variations in other languages.
+export const OriginalMessage = new RegExp(
+  `[\s]*[-]+[ ]*(${
+    [
+      "Original Message", "Reply Message",            // English
+      "Ursprüngliche Nachricht", "Antwort Nachricht", // German
+      "Oprindelig meddelelse"                         // Danish
+    ].join("|")
+  })[ ]*[-]+`, "i"
+);
+
+export const FromColonOrDateColon = new RegExp(
+  `(_+\r?\n)?[\s]*(:?[*]?${
+    [
+      "From", "Van", "De", "Von", "Fra", "Från",      // "From" in different languages.
+      "Date", "Datum", "Envoyé", "Skickat", "Sendt",  // "Date" in different languages.
+    ].join("|")
+  })[\s]?:[*]? .*`, "i"
+);
+
+export const SplitterPatterns = [
+  OriginalMessage,
+  OnDateSomebodyWrote,
+  OnDateWroteSomebody,
+  FromColonOrDateColon,
+  // 02.04.2012 14:20 пользователь "bob@example.com" <
+  // bob@xxx.mailgun.org> написал:
+  new RegExp("(\d+/\d+/\d+|\d+\[^]\d+\[^]\d+)[^]*@"),
+  // 2014-10-17 11:28 GMT+03:00 Bob <
+  // bob@example.com>:
+  new RegExp("\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}\s+GMT[^]*@"),
+  // Thu, 26 Jun 2014 14:00:51 +0400 Bob <bob@example.com>:
+  new RegExp("\S{3,10}, \d\d? \S{3,10} 20\d\d,? \d\d?:\d\d(:\d\d)?( \S+){3,6}@\S+:"),
+  // Sent from Samsung MobileName <address@example.com> wrote:
+  new RegExp("Sent from Samsung .*@.*> wrote")
+];
+
+export const Link = new RegExp("<(http://[^>]*)>");
+export const NormalizedLink = new RegExp("@@(http://[^>@]*)@@");
+export const ParenthesisLink = new RegExp("\(https?://");
+
+export const QuotePatten = new RegExp("^>+ ?");
+export const NoQuoteLine = new RegExp("^[^>].*[\S].*");
